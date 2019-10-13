ffa05cfaed44f79b904ed9650542ee546370903b
Viet Hoang Nguyen <Nogon-H.N@seznam.cz>
2019-10-14 01:53:38 +0200

    Branches

--- Game/main.cpp
+++ Game/main.cpp
@@ -102,52 +102,86 @@ public:
             cout << "A: I am going to open it. " << endl;
             cout << "B: I am not interested in the letter, I am searching for other things." << endl;
             cout << "C: I am going to head out, because I didn't find anything useful." << endl;
+            m_adv++;
         }else if(m_continue == "C" && m_adv == 3){
             cout << "You have lied down and started thinking about your meaningless life and your entire existence. What is your next move?" << endl;
             cout << "Press:" << endl;
             cout << "A: I am going to commit suicide." << endl;
             cout << "B: I am going to pray to Jesus." << endl;
             cout << "C: I am going to think about something nice." << endl;
+            m_adv++;
         }
     }
 
     // Continue 3(Adv == 3), A(Adv == 2) == Continue21A
     void getContinue3A(){
         if (m_continue == "A" && m_adv == 3){
-            cout << "You are trying to find a weapon, but it's dark.. GRROOOOOWLL.. Suddenly you are feeling chills all over your body. \"Something\" is getting closer.. What is your next move? " << endl;
+            cout << "You are trying to find a weapon, but it's too dark.. GRROOOOOWWLL.. Suddenly you are feeling chills all over your body. \"Something\" is getting closer.. What is your next move? " << endl;
             cout << "Press:" << endl;
             cout << "A: I am going to panic and scream for help." << endl;
             cout << "B: I am determined to fight it." << endl;
             cout << "C: I am starting to have seizure." << endl;
+            m_adv++;
         }else if(m_continue == "B" && m_adv == 3){
             cout << "You are calling for help, but there is no response. \"Something\" has heard you, and it's running fast towards you.  What is your next move?" << endl;
             cout << "Press:" << endl;
             cout << "A: I am determined to fight. " << endl;
             cout << "B: I am going to use magic, even though I don't know any." << endl;
             cout << "C: I feel like I am going to die." << endl;
+            m_adv++;
         }else if(m_continue == "C" && m_adv == 3){
             cout << "You are checking your pockets just to find a key in them. The key has a weird shape like a dragon's shape. You feel some magic power coming out from it. What is your next move?" << endl;
             cout << "Press:" << endl;
             cout << "A: I am going to look for any keyhole." << endl;
             cout << "B: I am going to try absorb the magic from it. " << endl;
             cout << "C: I am going to throw it away." << endl;
-        }
-    }
-    // Adventure begins
-    void getBegin(){
-        if(m_adv == 1) {
-            cout << "You have woken up in an unknown room. You are looking around, but you don't see anything interesting. What are you going to do?" << endl;
-            cout << "Press:" << endl;
-            cout << "A: I am going to head outside." << endl;
-            cout << "B: I am going to inspect the room in details." << endl;
-            cout << "C: I am not planning to do anything." << endl;
             m_adv++;
-
-        }else{
-            cout << "You are not allowed to play. Restart the game!" << endl;
         }
-
     }
+
+    void getContinue3B() {
+        if (m_continue == "A" && m_adv == 3) {
+            cout << "You have opened the letter. There is something written on it. "<< endl;
+            cout << "\"I have been born in a small town called Worzei near Bress Hills. I came from a poor family. All I had was my younger brother Derun. I was the one who has taken care of him all the time like he was my child. After I turned 30, I found out that, my parents were evil beings which have brought a disaster towards humanity... \"" << endl;
+            cout << "Press:" << endl;
+            cout << "A: I am going to throw the letter away." << endl;
+            cout << "B: I am feeling sad, still gonna throw the letter away.." << endl;
+            cout << "C: I am going to store the letter in my pocket." << endl;
+            m_adv++;
+        } else if (m_continue == "B" && m_adv == 3) {
+            cout << "You are searching for everything in the room to find some clues. All you can find there are spider webs, rotten fruits and some bread."<< endl;
+            cout << "Press:" << endl;
+            cout << "A: I am greatly disappointed. I am going to head outside. " << endl;
+            cout << "B: I am going to store some bread in my pocket." << endl;
+            cout << "C: I am going to store some rotten fruits in my pocket." << endl;
+            m_adv++;
+        } else if (m_continue == "C" && m_adv == 3) {
+            cout << "When you are about to step out of the room, you happen to trip over \"something\".. You get up and look at that thing. There is an invisible thing on the ground, you don't see it,but you feel it. " << endl;
+            cout << "Press:" << endl;
+            cout << "A: I am going to pick it up." << endl;
+            cout << "B: I am going to ignore it. " << endl;
+            cout << "C: I am going to inspect it more throughly." << endl;
+            m_adv++;
+        }
+    }
+
+        // Adventure begins
+        void getBegin() {
+            if (m_adv == 1) {
+                cout
+                        << "You have woken up in an unknown room. You are looking around, but you don't see anything interesting. What are you going to do?"
+                        << endl;
+                cout << "Press:" << endl;
+                cout << "A: I am going to head outside." << endl;
+                cout << "B: I am going to inspect the room in details." << endl;
+                cout << "C: I am not planning to do anything." << endl;
+                m_adv++;
+
+            } else {
+                cout << "You are not allowed to play. Restart the game!" << endl;
+            }
+
+        }
 
 
 };
@@ -183,7 +217,11 @@ int main() {
     newChar->getContinue2();
     getline(cin,continuee);
     newChar->setContinue(continuee);
-    newChar->getContinue3A();
+    if(continuee == "A"){
+        newChar->getContinue3A();
+    }else if(continuee == "B"){
+        newChar->getContinue3B();
+    }
 
     return 0;
 }
