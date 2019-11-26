af671f425a0c9d6a9232a72ab7934d5e66b48ee2
Viet Hoang Nguyen <Nogon-H.N@seznam.cz>
2019-11-27 00:41:07 +0100

    2nd step

--- NewGame/main.cpp
+++ NewGame/main.cpp
@@ -3,7 +3,25 @@
 #include <ctime>
 #include <cstdlib>
 
-class Player{
+class Weapon {
+private:
+    int m_dmg;
+    std::string m_name;
+
+public:
+    Weapon(int dmg, const std::string& name) {
+        m_dmg = dmg;
+        m_name = name;
+    }
+    int getDamage() {
+        return m_dmg;
+    }
+    std::string getName() {
+        return m_name;
+    }
+};
+
+class Character {
 private:
     int m_hp;
     int m_mp;
@@ -11,9 +29,11 @@ private:
     int m_agility;
     int m_vitality;
     int m_intelligence;
+    Weapon m_currWeapon;
+    int m_dmg;
 
 public:
-    Player(){
+    Character() : m_currWeapon(0, "Fists"){
         std::srand(time(nullptr));
         m_hp = m_vitality * m_vitality;
         m_mp = m_intelligence * m_intelligence;
@@ -21,9 +41,33 @@ public:
         m_agility = std::rand () % 11;
         m_vitality = std::rand () % 11;
         m_intelligence = std::rand () % 11;
+        m_dmg = m_strength * m_strength;
+    }
+    void setWeapon(const Weapon& weapon) {
+        m_currWeapon = weapon;
+    }
+    int getDamage(){
+        return m_dmg + m_currWeapon.getDamage();
     }
 };
-class Monster{
+
+static void chooseWeapon(Character* character){
+        std::vector<Weapon*> m_weapons;
+
+        m_weapons.push_back(new Weapon(10, "Bronze Sword"));
+        m_weapons.push_back(new Weapon(20, "Silver Sword"));
+        m_weapons.push_back(new Weapon(150, "Sword of Justice"));
+
+        std::cout << "Choose a weapon: \n";
+        for (size_t i = 0; i < m_weapons.size(); ++i)
+            std::cout << i << ". " << m_weapons[i]->getName() << "\n";
+
+        size_t choice;
+        std::cin >> choice;
+        character->setWeapon(m_weapons[choice]);
+};
+
+class Monster {
 private:
     int m_hp;
     int m_mp;
@@ -33,10 +77,20 @@ private:
     int m_intelligence;
 
 public:
-
+    Monster(){
+        std::srand(time(nullptr));
+        m_hp = m_vitality * m_vitality;
+        m_mp = m_intelligence * m_intelligence;
+        m_strength = std::rand () % 21;
+        m_agility = std::rand () % 21;
+        m_vitality = std::rand () % 21;
+        m_intelligence = std::rand () % 21;
+    }
 };
 
 int main() {
+    Character* player = new Character();
+    chooseWeapon(player);
 
     return 0;
 }
