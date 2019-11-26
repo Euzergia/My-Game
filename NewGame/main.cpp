91d365d98debcede3c598cda31f41a1edecac347
Viet Hoang Nguyen <Nogon-H.N@seznam.cz>
2019-11-26 22:15:42 +0100

    1st step

--- NewGame/main.cpp
+++ NewGame/main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+
+class Player{
+private:
+    int m_hp;
+    int m_mp;
+    int m_strength;
+    int m_agility;
+    int m_vitality;
+    int m_intelligence;
+
+public:
+    Player(){
+        std::srand(time(nullptr));
+        m_hp = m_vitality * m_vitality;
+        m_mp = m_intelligence * m_intelligence;
+        m_strength = std::rand () % 11;
+        m_agility = std::rand () % 11;
+        m_vitality = std::rand () % 11;
+        m_intelligence = std::rand () % 11;
+    }
+};
+class Monster{
+private:
+    int m_hp;
+    int m_mp;
+    int m_strength;
+    int m_agility;
+    int m_vitality;
+    int m_intelligence;
+
+public:
+
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
