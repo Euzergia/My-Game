d7776edbe7b7a8f9a01d2db04d106da2343b48d6
Viet Hoang Nguyen <Nogon-H.N@seznam.cz>
2019-12-01 23:34:16 +0100

    4th step

--- NewGame/main.cpp
+++ NewGame/main.cpp
@@ -31,23 +31,30 @@ private:
     int m_intelligence;
     Weapon m_currWeapon;
     int m_dmg;
+    std::string m_weaponName;
 
 public:
     Character() : m_currWeapon(0, "Fists"){
         std::srand(time(nullptr));
-        m_hp = m_vitality * m_vitality;
-        m_mp = m_intelligence * m_intelligence;
         m_strength = std::rand () % 11;
         m_agility = std::rand () % 11;
         m_vitality = std::rand () % 11;
         m_intelligence = std::rand () % 11;
         m_dmg = m_strength * m_strength;
+        m_hp = m_vitality * m_vitality;
+        m_mp = m_intelligence * m_intelligence;
     }
     void setWeapon(const Weapon& weapon) {
         m_currWeapon = weapon;
     }
     int getDamage() const {
         return m_dmg + m_currWeapon.getDamage();
+    }
+    void setName(std::string wname){
+        m_weaponName = wname;
+    }
+    std::string getName(){
+        return m_weaponName;
     }
 };
 
@@ -63,8 +70,11 @@ static void chooseWeapon(Character* character){
             std::cout << i << ". " << m_weapons[i].getName() << "\n";
 
         size_t choice;
+        std::string weapon;
         std::cin >> choice;
         character->setWeapon(m_weapons[choice]);
+        weapon = m_weapons[choice].getName();
+        character->setName(weapon);
 };
 
 class Monster {
@@ -79,18 +89,45 @@ private:
 public:
     Monster(){
         std::srand(time(nullptr));
-        m_hp = m_vitality * m_vitality;
-        m_mp = m_intelligence * m_intelligence;
         m_strength = std::rand () % 21;
         m_agility = std::rand () % 21;
         m_vitality = std::rand () % 21;
         m_intelligence = std::rand () % 21;
+        m_hp = m_vitality * m_vitality * 3;
+        m_mp = m_intelligence * m_intelligence * 3;
+    }
+    int getRand(){
+        return m_vitality;
+    }
+    int getHp(){
+        return m_hp;
+    }
+};
+
+class Fight {
+    std::string input = "";
+public:
+    int getFight(Monster* monster, Character* character){
+        int hp = monster->getHp();
+        while(hp > 0) {
+            std::cout << "Weapon: " << character->getName() << std::endl;
+            std::cout << "Leo " << hp << std::endl;
+            std::cin >> input;
+            if (input == "a" || input == "A") {
+                hp -= character->getDamage();
+            }
+        }
+        std::cout << "Leo " << hp << " is dead.\n";
+        return hp;
     }
 };
 
 int main() {
     Character* player = new Character();
+    Monster* monster = new Monster();
+    Fight* fight = new Fight;
     chooseWeapon(player);
+    fight->getFight(monster, player);
 
     return 0;
 }
