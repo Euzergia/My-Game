e9a4f2e2a7bcc25b46639259c3f7255fc82d47fc
Viet Hoang Nguyen <Nogon-H.N@seznam.cz>
2019-12-02 01:10:33 +0100

    5th step

--- NewGame/main.cpp
+++ NewGame/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <ctime>
 #include <cstdlib>
+#include <array>
 
 class Weapon {
 private:
@@ -31,6 +32,7 @@ private:
     int m_intelligence;
     Weapon m_currWeapon;
     int m_dmg;
+    std::string m_name;
     std::string m_weaponName;
 
 public:
@@ -40,9 +42,19 @@ public:
         m_agility = std::rand () % 11;
         m_vitality = std::rand () % 11;
         m_intelligence = std::rand () % 11;
+        m_name = "";
         m_dmg = m_strength * m_strength;
         m_hp = m_vitality * m_vitality;
         m_mp = m_intelligence * m_intelligence;
+    }
+    void setName(){
+        std::string name;
+        std::cout << "Enter your nickname: \n";
+        std::cin >> name;
+        m_name = name;
+    }
+    std::string getName(){
+        return m_name;
     }
     void setWeapon(const Weapon& weapon) {
         m_currWeapon = weapon;
@@ -50,11 +62,20 @@ public:
     int getDamage() const {
         return m_dmg + m_currWeapon.getDamage();
     }
-    void setName(std::string wname){
+    int getBaseDmg(){
+        return  m_dmg;
+    }
+    void setWeaponName(std::string wname){
         m_weaponName = wname;
     }
-    std::string getName(){
+    std::string getWeaponName(){
         return m_weaponName;
+    }
+    int getHp(){
+        return m_hp;
+    }
+    int getMp(){
+        return m_mp;
     }
 };
 
@@ -63,7 +84,10 @@ static void chooseWeapon(Character* character){
 
         m_weapons.push_back(Weapon(10, "Bronze Sword"));
         m_weapons.push_back(Weapon(20, "Silver Sword"));
-        m_weapons.push_back(Weapon(150, "Sword of Justice"));
+        m_weapons.push_back(Weapon(50, "Sword of Justice"));
+        m_weapons.push_back(Weapon(80, "Excalibur"));
+        m_weapons.push_back(Weapon(110, "Hellbringer"));
+        m_weapons.push_back(Weapon(150, "Heaven's Wish"));
 
         std::cout << "Choose a weapon: \n";
         for (size_t i = 0; i < m_weapons.size(); ++i)
@@ -74,7 +98,7 @@ static void chooseWeapon(Character* character){
         std::cin >> choice;
         character->setWeapon(m_weapons[choice]);
         weapon = m_weapons[choice].getName();
-        character->setName(weapon);
+        character->setWeaponName(weapon);
 };
 
 class Monster {
@@ -85,6 +109,8 @@ private:
     int m_agility;
     int m_vitality;
     int m_intelligence;
+    std::array<std::string, 5> m_monsterName;
+    int m_randIndex;
 
 public:
     Monster(){
@@ -95,39 +121,89 @@ public:
         m_intelligence = std::rand () % 21;
         m_hp = m_vitality * m_vitality * 3;
         m_mp = m_intelligence * m_intelligence * 3;
+        m_randIndex = rand() % 6;
+        m_monsterName = {"Goblin", "Dragon", "Joker", "Evil King", "Wyvern"};
     }
-    int getRand(){
-        return m_vitality;
+    int setHp(int hp){
+        m_hp = hp;
     }
     int getHp(){
         return m_hp;
+    }
+    std::string getName(){
+        return m_monsterName[m_randIndex];
     }
 };
 
 class Fight {
     std::string input = "";
 public:
+    void line(){
+        std::cout << "--------------------------\n";
+    }
     int getFight(Monster* monster, Character* character){
         int hp = monster->getHp();
         while(hp > 0) {
-            std::cout << "Weapon: " << character->getName() << std::endl;
-            std::cout << "Leo " << hp << std::endl;
+            std::cout << "Weapon: " << character->getWeaponName() << std::endl;
+            std::cout << monster->getName() << " " << hp << std::endl;
             std::cin >> input;
-            if (input == "a" || input == "A") {
+            if (input == "a" || input == "A"){
                 hp -= character->getDamage();
+                std::cout << "Name: " << character->getName() << std::endl;
+                std::cout << "HP: " << character->getHp() << std::endl;
+                std::cout << "MP: " << character->getMp() << std::endl;
+                std::cout << "Dmg: " << character->getDamage() << std::endl;
+                std::cout << "Base Dmg: " << character->getBaseDmg() << std::endl;
             }
         }
-        std::cout << "Leo " << hp << " is dead.\n";
+        if(hp < 0){
+            std::cout << monster->getName() << " " << hp << " is dead.\n";
+            std::cout << "Overkill!!\n";
+            monster->setHp(hp);
+        }else {
+            std::cout << monster->getName() << " " << hp << " is dead.\n";
+            monster->setHp(hp);
+        }
+        line();
+
         return hp;
     }
 };
 
 int main() {
+    bool again = false;
+    std::string input;
+    std::string playerName;
     Character* player = new Character();
     Monster* monster = new Monster();
     Fight* fight = new Fight;
+    player->setName();
     chooseWeapon(player);
     fight->getFight(monster, player);
+    delete monster;
+    std::cout << "Do you want to fight another monster??\n" << "A: Yes\n" << "B: No\n";
+    std::cin >> input;
+    if(input == "a" || input == "A"){
+        again = true;
+        while(again){
+            Monster* monster = new Monster();
+            while(monster->getHp() > 0){
+                fight->getFight(monster, player);
+            }
+            delete monster;
+            std::cout << "Do you want to fight another monster??\n" << "A: Yes\n" << "B: No\n";
+            std::cin >> input;
+            if(input == "b" || input == "B"){
+                again = false;
+            }
+        }
+    }else {
+        std::cout << "The game has ended.\n";
+    }
+
+    delete player;
+    delete monster;
+    delete fight;
 
     return 0;
 }
